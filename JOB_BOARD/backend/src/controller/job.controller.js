f927b3f4746b8c014e3c178dc1c63a1dab8dd987
ikeshav26 <keshavgilhotra4@gmail.com>
2025-08-01 01:07:12 +0530

    ✨ Fetching created jobs

--- JOB_BOARD/backend/src/controller/job.controller.js
+++ JOB_BOARD/backend/src/controller/job.controller.js
@@ -3,7 +3,7 @@ import Job from '../models/job.model.js'
 export const createJob=async(req,res)=>{
     try{
         const role = req.user.role;
-        if(role !== 'admin'){
+        if(role !== 'employer'){
             return res.status(403).json({ message: 'Access denied. Only admins can create jobs.' });
         }
 
@@ -24,6 +24,38 @@ export const createJob=async(req,res)=>{
         res.status(201).json({ message: 'Job created successfully', job: populatedJob });
     }catch(err){
         res.status(500).json({ message: 'Error creating job', error: err.message });
+        console.error(err);
+    }
+}
+
+
+export const getEmployerCreatedJobs=async(req,res)=>{
+    try{
+        const employerId = req.user.id;
+        if(req.user.role !== 'employer'){
+            return res.status(403).json({ message: 'Access denied. Only employers can view their created jobs.' });
+        }
+        const jobs = await Job.find({ postedBy: employerId }).populate('postedBy', 'username email');
+        if(jobs.length === 0){
+            return res.status(404).json({ message: 'No jobs found for this employer.' });
+        }
+        res.status(200).json({ message: 'Jobs fetched successfully', jobs });
+    }catch(err){
+        res.status(500).json({ message: 'Error fetching jobs', error: err.message });
+        console.error(err);
+    }
+}
+
+
+export const getAllJobs=async(req,res)=>{
+    try{
+        const jobs=await Job.find().populate('postedBy', 'username email').sort({ createdAt: -1 });
+        if(jobs.length === 0){
+            return res.status(404).json({ message: 'No jobs available.' });
+        }
+        res.status(200).json({ message: 'Jobs fetched successfully', jobs });
+    }catch(err){
+        res.status(500).json({ message: 'Error fetching jobs', error: err.message });
         console.error(err);
     }
 }
--- JOB_BOARD/backend/src/routes/job.routes.js
+++ JOB_BOARD/backend/src/routes/job.routes.js
@@ -1,11 +1,13 @@
 import express from 'express';
-import { createJob } from '../controller/job.controller.js';
+import { createJob, getAllJobs, getEmployerCreatedJobs } from '../controller/job.controller.js';
 import  userAuth  from '../middlewares/userAuth.js';
 
 const router=express.Router();
 
 
 router.post('/create-job',userAuth,createJob)
+router.get('/employer-jobs',userAuth,getEmployerCreatedJobs)
+router.get('/all-jobs', userAuth, getAllJobs)
 
 
 export default router;
