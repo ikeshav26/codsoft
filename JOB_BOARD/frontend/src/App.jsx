685e08aec61669033d254fc1f1e04b9758d4ce12
ikeshav26 <keshavgilhotra4@gmail.com>
2025-08-01 13:09:53 +0530

    ✨ creating routes and settingup context

--- JOB_BOARD/frontend/src/App.jsx
+++ JOB_BOARD/frontend/src/App.jsx
@@ -1,9 +1,35 @@
-import React from 'react'
+import React, { useContext } from 'react'
+import AppContext from './context/AppContext'
+import Navbar from './components/Navbar'
+import Footer from './components/Footer'
+import { Routes,Route, Navigate } from 'react-router-dom'
+import Home from './pages/Home'
+import Login from './pages/Login'
+import Signup from './pages/Signup'
+import EmployerDashboard from './pages/EmployerDashboard'
+import UserDashboard from './pages/UserDashboard'
+import Contact from './pages/Contact'
+import ResetPassword from './pages/ResetPassword'
+import CreateJob from './pages/CreateJob'
+import ApplyJob from './pages/ApplyJob'
 
 const App = () => {
+  const {user,navigate,employer,location}=useContext(AppContext);
   return (
     <div className='bg-red-400 text-3xl'>
-      JOB board website
+      {location.pathname.includes('login') || location.pathname.includes('signup') ? null : <Navbar/>}
+      <Routes>
+        <Route path="/" element={<Home/>}/>
+        <Route path="/login" element={<Login/>}/>
+        <Route path="/signup" element={<Signup/>}/>
+        <Route path="/employer-dashboard" element={employer?<EmployerDashboard/>:<Navigate to='/'/>}/>
+        <Route path="/user-dashboard" element={user?<UserDashboard/>:<Navigate to='/'/>}/>
+        <Route path="/contact" element={<Contact/>}/>
+        <Route path="/reset-password" element={<ResetPassword/>}/>
+        <Route path="/create-job" element={employer?<CreateJob/>:<Navigate to='/'/>}/>
+        <Route path="/apply-job" element={user?<ApplyJob/>:<Navigate to='/'/>}/>
+      </Routes>
+      {location.pathname.includes('login') || location.pathname.includes('signup') ? null : <Footer/>}
     </div>
   )
 }
---
+++ JOB_BOARD/frontend/src/context/AppContext.jsx
@@ -0,0 +1,23 @@
+import { createContext, useState } from "react";
+import { useLocation, useNavigate } from "react-router-dom";
+
+
+
+const AppContext=createContext();
+
+
+export const ContextProvider=({children})=>{
+    const [user, setuser] = useState(localStorage.getItem('user') ? JSON.parse(localStorage.getItem('user')) : null);
+    const [employer, setemployer] = useState(localStorage.getItem('employer') ? JSON.parse(localStorage.getItem('employer')) : null);
+    const [jobs, setjobs] = useState([])
+    const navigate=useNavigate();
+    const location=useLocation();
+    const value={user,setuser,employer,setemployer,navigate,jobs,setjobs,location};
+    return(
+        <AppContext.Provider value={value}>
+            {children}
+        </AppContext.Provider>
+    )
+}
+
+export default AppContext;
