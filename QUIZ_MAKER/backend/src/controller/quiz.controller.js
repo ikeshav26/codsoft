10629034285f3a2ebdae0076d63028e7e12aba96
ikeshav26 <keshavgilhotra4@gmail.com>
2025-08-03 22:40:47 +0530

    ✨ Fetching user quizes

--- QUIZ_MAKER/backend/src/controller/quiz.controller.js
+++ QUIZ_MAKER/backend/src/controller/quiz.controller.js
@@ -72,3 +72,31 @@ export const allQuizes=async(req,res)=>{
         res.status(500).json({message: 'Internal server error'});   
     }
 }
+
+
+export const getQuizById=async(req,res)=>{
+    try{
+        const {id}=req.params;
+        const quiz=await Quiz.findById(id).populate('createdBy', 'username').populate('questions');
+        if(!quiz){
+            return res.status(404).json({message: 'Quiz not found'});
+        }
+        res.status(200).json({message: 'Quiz fetched successfully', quiz});
+    }catch(err){
+        console.error(err);
+        res.status(500).json({message: 'Internal server error'});
+    }
+}
+
+
+export const getUserCreatedQuizes=async(req,res)=>{
+    try{
+        console.log("Fetching user created quizzes for user ID:", req.user);
+        const userId=req.user;
+        const quizes=await Quiz.find({createdBy: userId}).sort({createdAt: -1});
+        res.status(200).json({message: 'User created quizzes fetched successfully', quizes});
+    }catch(err){
+        console.error(err);
+        res.status(500).json({message: 'Internal server error'});
+    }
+}
--- QUIZ_MAKER/backend/src/routes/quiz.routes.js
+++ QUIZ_MAKER/backend/src/routes/quiz.routes.js
@@ -1,13 +1,16 @@
 import express from 'express';
-import { addQuestionToQuiz, allQuizes, createQuiz } from '../controller/quiz.controller.js';
+import { addQuestionToQuiz, allQuizes, createQuiz, getQuizById, getUserCreatedQuizes } from '../controller/quiz.controller.js';
 import  {userAuth} from '../middlewares/user.auth.js';
 
 const router=express.Router();
 
 
+router.get('/my-quizes',userAuth,getUserCreatedQuizes)
+router.get('/:id', userAuth,getQuizById)
 router.post('/create',userAuth,createQuiz)
 router.get('/all-quizes',userAuth,allQuizes)
 router.post('/add-question/:id',userAuth,addQuestionToQuiz)
+
 
 
 export default router;
