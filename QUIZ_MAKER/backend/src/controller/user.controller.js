f92ba43a83dafcbcbdb0c8fcdf60b483234fa0e9
ikeshav26 <keshavgilhotra4@gmail.com>
2025-08-03 17:20:34 +0530

    ✨ User login and logout routes

--- QUIZ_MAKER/backend/src/controller/user.controller.js
+++ QUIZ_MAKER/backend/src/controller/user.controller.js
@@ -17,10 +17,47 @@ export const signup=async(req,res)=>{
 
         const hashedPassword=await bcrypt.hash(password, 10);
         const newUser=await User.create({name,email,password:hashedPassword});
-        const token=jwt.sign({ id:newUser._id }, process.env.JWT_SECRET, { expiresIn: '1d' });
-        res.cookie('token', token, { httpOnly: true, secure: process.env.NODE_ENV === 'production',sameSite:'None' });
+        const token=jwt.sign({ id:newUser._id }, process.env.JWT_SECRET, { expiresIn: '7d' });
+        res.cookie('token', token, { httpOnly: true, secure: process.env.NODE_ENV === 'production', });
 
         res.status(201).json({message:"User created successfully", user: newUser, token });
+    }catch(err){
+        console.error(err);
+        res.status(500).json({ message: 'Internal server error' });
+    }
+}
+
+export const login=async(req,res)=>{
+    try{
+        const {email,password}=req.body;
+        if(!email || !password){
+            return res.status(400).json({ message: 'All fields are required' });
+        }
+
+        const user=await User.findOne({email});
+        if(!user){
+            return res.status(400).json({ message: 'Invalid credentials' });
+        }
+
+        const isPasswordValid=await bcrypt.compare(password, user.password);
+        if(!isPasswordValid){
+            return res.status(400).json({ message: 'Invalid credentials' });
+        }
+
+        const token=jwt.sign({id:user._id }, process.env.JWT_SECRET, { expiresIn: '7d' });
+        res.cookie('token',token, { httpOnly: true, secure: process.env.NODE_ENV === 'production' });
+
+        res.status(200).json({ message: 'Login successful', user, token });
+    }catch(err){
+        console.error(err);
+        res.status(500).json({ message: 'Internal server error' });
+    }
+}
+
+export const logout=async(req,res)=>{
+    try{
+        res.clearCookie('token');
+        res.status(200).json({ message: 'Logout successful' });
     }catch(err){
         console.error(err);
         res.status(500).json({ message: 'Internal server error' });
--- QUIZ_MAKER/backend/src/routes/user.routes.js
+++ QUIZ_MAKER/backend/src/routes/user.routes.js
@@ -1,10 +1,12 @@
 import express from 'express';
-import { signup } from '../controller/user.controller.js';
+import { login, logout, signup } from '../controller/user.controller.js';
 
 
 const router=express.Router();
 
 router.post('/signup',signup)
+router.post('/login',login)
+router.get('/logout',logout)
 
 
 export default router;
